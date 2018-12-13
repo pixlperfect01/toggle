aadca14cdab44426291a61ac68b0f50658871530
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-13 10:27:18 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html lang="en">
-    <head></head>
+    <head><meta content-type="text/html" charset="UTF-8"></head>
     <body><div id="content" hidden><?php $homepage = file_get_contents('http://www.google.com');echo $homepage; ?></div>
     
 </body>
