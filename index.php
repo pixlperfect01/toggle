f0d8a74f76fb4cd08bb5c35ae6997a0051ffac9a
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-14 09:31:41 -0600

    Update and rename index.php to index.html

--- index.php
+++
@@ -1,6 +0,0 @@
-<html lang="en">
-    <head><meta content-type="text/html" charset="UTF-8"></head>
-    <body><div id="content" hidden><?php $homepage = file_get_contents('http://www.google.com');echo $homepage; ?></div>
-    
-</body>
-</html>
